0dcf53737b784a892f53de22dbda48f652c2ee90
KimYoungWook730 <wook5272@naver.com>
2025-06-09 20:15:29 +0900

    Add files via upload

---
+++ Linux_Final_Test/update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+API_KEY="시크릿키"
+LAWD_CD="41173"
+DEAL_YMD="202505"
+SAVE_DIR="/data/xml"
+
+mkdir -p "$SAVE_DIR"
+
+for PAGE in 1 2 3 4
+do
+  for MONTH in 1 2 3 4 5 6
+  do
+  curl "https://apis.data.go.kr/1613000/RTMSDataSvcAptTrade/getRTMSDataSvcAptTrade?serviceKey=${API_KEY}&LAWD_CD=${LAWD_CD}&DEAL_YMD=20250${MONTH}&pageNo=${PAGE}&numOfRows=10"\
+  -H "Accept: application/xml"\
+  -o "${SAVE_DIR}/data${MONTH}-${PAGE}.xml"
+  done
+done
